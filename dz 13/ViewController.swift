eb66f3ad22c184b1a4656752aa2b58fc55e422e2
Memka <mkasenova87@gmail.com>
2023-03-08 22:28:06 +0600

    исправ

--- dz 13/ViewController.swift
+++ dz 13/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoView: UIImageView!
+    
+    @IBOutlet weak var vxodLabel: UILabel!
+    
+    @IBOutlet weak var NameTF: UITextField!
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    @IBOutlet weak var KasenovaTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButton(_ sender: Any) {
+        
+        if NameTF.text?.isEmpty ?? true {
+            NameTF.layer.borderColor =  UIColor.red.cgColor
+            NameTF.layer.borderWidth = 5
+            NameTF.placeholder = "fill the field"
+                } else {
+                    NameTF.layer.borderWidth = 0
+                }
+    }
+    
+    @IBAction func LoginButton2(_ sender: Any) {
+        let login = KasenovaTF.text ?? "empty"
+        
+            KasenovaTF.layer.cornerRadius = 5
+            KasenovaTF.layer.borderColor = UIColor.red.cgColor
+        KasenovaTF.layer.borderWidth = 3
+    }
 }
 
